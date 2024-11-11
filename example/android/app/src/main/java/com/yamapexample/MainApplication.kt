91cca2b6914d7e20bab4c7defa71ae27a1900952
Aleksey Pekhterev <qudaeo@yandex.ru>
2024-11-11 11:52:34 +0700

    feat: add setLocale in android

--- example/android/app/src/main/java/com/yamapexample/MainApplication.kt
+++ example/android/app/src/main/java/com/yamapexample/MainApplication.kt
@@ -11,6 +11,8 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
+import com.yandex.mapkit.MapKitFactory
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -34,6 +36,9 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+
+    MapKitFactory.setLocale("ru_RU")
+
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
