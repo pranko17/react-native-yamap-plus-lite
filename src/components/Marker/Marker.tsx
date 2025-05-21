14bab66cd40106e8389604054ff70cc142c79ad4
Пехтерев Алексей Сергеевич <pehterev.a@galero.tech>
2025-05-21 14:27:49 +0700

    refactor: add useMarker hook

--- src/components/Marker/Marker.tsx
+++ src/components/Marker/Marker.tsx
@@ -1,44 +1,19 @@
-import React, {forwardRef, useCallback, useImperativeHandle, useMemo, useRef} from 'react';
-import {Platform, UIManager, findNodeHandle, Image} from 'react-native';
-import {Point} from '../../interfaces';
-import {MarkerNativeComponent, MarkerNativeRef} from './MarkerNativeComponent';
+import React, {forwardRef, useMemo} from 'react';
+import {MarkerNativeComponent} from './MarkerNativeComponent';
 import {MarkerProps, MarkerRef} from './types';
-
-const getCommand = (cmd: string) => {
-  return Platform.OS === 'ios' ? UIManager.getViewManagerConfig('YamapMarker').Commands[cmd] : cmd;
-};
+import {useMarker} from '../../hooks/useMarker';
+import {getImageUri} from '../../utils';
 
 export const Marker = forwardRef<MarkerRef, MarkerProps>(({source, ...props}, ref) => {
-  const nativeRef = useRef<MarkerNativeRef | null>(null);
+  const {nativeRef} = useMarker(ref);
 
-  const resolvedSource = useMemo(() => source ? Image.resolveAssetSource(source) : undefined, [source]);
-
-  const animatedMoveTo = useCallback((coords: Point, duration: number) => {
-    UIManager.dispatchViewManagerCommand(
-      findNodeHandle(nativeRef.current),
-      getCommand('animatedMoveTo'),
-      [coords, duration]
-    );
-  }, []);
-
-  const animatedRotateTo = useCallback((angle: number, duration: number) => {
-    UIManager.dispatchViewManagerCommand(
-      findNodeHandle(nativeRef.current),
-      getCommand('animatedRotateTo'),
-      [angle, duration]
-    );
-  }, []);
-
-  useImperativeHandle(ref, () => ({
-    animatedMoveTo,
-    animatedRotateTo,
-  }), [animatedMoveTo, animatedRotateTo]);
+  const imageUri = useMemo(() => getImageUri(source), [source]);
 
   return (
     <MarkerNativeComponent
       {...props}
       ref={nativeRef}
-      source={resolvedSource?.uri}
+      source={imageUri}
       pointerEvents="none"
     />
   );
