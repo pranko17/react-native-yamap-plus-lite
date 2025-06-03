dfc20489eb030a5ad2a39424023ad42ca844eb01
Aleksey Pekhterev <qudaeo@yandex.ru>
2025-06-03 16:54:56 +0700

    refactor: rename YamapPackage

--- android/src/main/java/ru/vvdev/yamap/YamapPackage.kt
+++ android/src/main/java/ru/vvdev/yamap/YamapPackage.kt
@@ -1,20 +1,18 @@
 package ru.vvdev.yamap
 
 import com.facebook.react.ReactPackage
-import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 import ru.vvdev.yamap.search.RNYandexSearchModule
 import ru.vvdev.yamap.suggest.RNYandexSuggestModule
 
-class RNYamapPackage : ReactPackage {
-    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(
-            YamapModule(reactContext),
-            RNYandexSuggestModule(reactContext),
-            RNYandexSearchModule(reactContext)
-        )
-    }
+class YamapPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext) = listOf(
+        YamapModule(reactContext),
+        RNYandexSuggestModule(reactContext),
+        RNYandexSearchModule(reactContext)
+    )
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
         return listOf(
