ef748b88c0e328d45fccd35d361c43eb88a71995
Aleksey Pekhterev <qudaeo@yandex.ru>
2025-06-26 11:23:24 +0700

    feat: remove ios app init in AppDelegate

--- example/ios/YamapExample/AppDelegate.swift
+++ example/ios/YamapExample/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import YandexMapsMobile
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -29,10 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       in: window,
       launchOptions: launchOptions
     )
-
-    YMKMapKit.setLocale("ru_RU")
-    YMKMapKit.setApiKey("Insert your MapKit SDK API KEY")
-    YMKMapKit.sharedInstance()
 
     return true
   }
